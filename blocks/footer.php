8a2b84275d7cea67c089bf2393a58d04a8f8b343
Daniel Mihai <163902601+messsimo@users.noreply.github.com>
2024-07-06 21:24:25 +0300

    Add files via upload

---
+++ blocks/footer.php
@@ -0,0 +1,47 @@
+<footer>
+    <div class="blocks">
+        <div class="block-1 main-block">
+            <h2>GamePortal</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+            <span>@GamePortal</span>
+        </div>
+
+        <div class="block-2 main-block">
+            <h3>About us</h3>
+            <a href="#">Zeux</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Careers</a>
+            <a href="#">Contact us</a>
+        </div>
+
+        <div class="block-3 main-block">
+            <h3>Contact us</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <p>+908 89097 890</p>
+        </div>
+
+        <div class="block-4 main-block">
+            <img src="images/facebook.png" alt="Facebook">
+            <img src="images/instagram.png" alt="Instagram">
+            <img src="images/x.png" alt="X">
+            <img src="images/linkedin.png" alt="LinkedIn">
+        </div>
+    </div>
+
+    <hr>
+    <span class="protect">Copyright ® 2024 All rights Rcerved</span>
+
+</footer>
+</div>
+
+<!-- Оповещение о не подключение JavaScript к браузеру -->
+<noscript>JavaScript dosen't work!</noscript>
+
+<!-- Подключение скрпита для валидации почты при рассылки -->
+<script src="/srcripts/loop.js"></script>
+
+<!-- Подключения скрпита для слайдера (About) -->
+<script src="/srcripts/slider.js"></script>
+
+</body>
+</html>
---
+++ blocks/loop.php
@@ -0,0 +1,18 @@
+    <!-- Секция с рассылкой -->
+    <div class="send">
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+
+        <div class="loop">
+            <div class="form">
+                <form id="form"> 
+                    <input type="text" placeholder="Enter email address" id="email">
+                    <button type="submit">Continue</button><br>
+                    <span id="error"></span>
+                </form>
+            </div>
+
+            <h2>Stay in the loop</h2>
+            <p>Subscribe to receive the latest news and updates about TDA. We promise not to spam you! </p>
+        </div>
+    </div>
